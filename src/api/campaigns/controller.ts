64f35ee6c66126cc536e05df04d317f6440b3021
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-02-01 21:41:17 +0530

    :sparkles: Add createCampaign endpoint

--- src/api/campaigns/controller.ts
+++ src/api/campaigns/controller.ts
@@ -1,5 +1,6 @@
 import database from '../../loaders/database';
 import LoggerInstance from '../../loaders/logger';
+import { Campaign } from '../../shared/customTypes';
 
 export const fetchCampaigns = async () => {
   try {
@@ -7,5 +8,17 @@ export const fetchCampaigns = async () => {
   } catch (error) {
     LoggerInstance.error(error);
     throw Error('Failed to get campaign data');
+  }
+};
+
+export const createCampaign = async (body: any) => {
+  try {
+    const newCampaign: Campaign = { ...body };
+    const databaseResponse = await (await database()).collection('compaign').findOne({ title: newCampaign.title });
+    if (databaseResponse !== undefined) throw 'Existing campaign with same title';
+    await (await database()).collection('campaign').insertOne(newCampaign);
+  } catch (error) {
+    LoggerInstance.error(error);
+    throw Error('Unable to create a new campaign');
   }
 };
--- src/api/campaigns/router.ts
+++ src/api/campaigns/router.ts
@@ -1,14 +1,24 @@
 import { Request, Response, Router } from 'express';
-import { fetchCampaigns } from './controller';
+import { createCampaign, fetchCampaigns } from './controller';
 
 const app = Router();
 export const campaignRouteHandler = () => {
   app.get('/', fetchCampaignsHandler);
+  app.post('/', createCampaignHandler);
   return app;
 };
 const fetchCampaignsHandler = async (req: Request, res: Response) => {
   try {
     res.json(await fetchCampaigns());
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+const createCampaignHandler = async (req: Request, res: Response) => {
+  try {
+    await createCampaign(req.body);
+    res.json({ success: true, message: 'Campaign was created successfully' });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
