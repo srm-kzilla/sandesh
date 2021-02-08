cf18b77d6a22f3e09d7dd390469ebe9f79225d17
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-02-08 21:01:12 +0530

    :recycle: Patch up router

--- src/api/auth/router.ts
+++ src/api/auth/router.ts
@@ -1,28 +1,27 @@
-import { Request,Response,Router } from 'express'
-import { createUser, authUser} from './controller'
-const app=Router();
+import { Request, Response, Router } from 'express';
+import { createUser, authUser } from './controller';
+const app = Router();
 
-export const authRouteHandler=()=>{
-    app.post("/login",loginHandler);
-    app.post("/signup",signUpHandler);
-    return app;
-}
+export const authRouteHandler = () => {
+  app.post('/login', loginHandler);
+  app.post('/signup', signUpHandler);
+  return app;
+};
 
-const loginHandler=async (req: Request,res: Response)=>{
-   try{
-      const accessToken=await authUser(req.body)
-      if(accessToken===null) throw Error('accessToken not assigned')
-      res.json({accessToken: accessToken})
-   }catch(error){
-       res.json({success: false,message: error})
-   }
-}
+const loginHandler = async (req: Request, res: Response) => {
+  try {
+    const jwt = await authUser(req.body);
+    res.json({ success: true, token: jwt });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-const signUpHandler=async (req: Request,res: Response)=>{
-    try{
-      await createUser(req.body)
-      res.json({success: true,message: 'User Successfully created'})
-    }catch(error){
-        res.json({success: false,message: error})
-    }
-}
+const signUpHandler = async (req: Request, res: Response) => {
+  try {
+    await createUser(req.body);
+    res.json({ success: true, message: 'User Successfully created' });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
