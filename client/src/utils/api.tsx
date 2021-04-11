50e693262646bb3d9b40746fcf1cff990ff5e394
Snehil <snehilwebber@gmail.com>
2021-04-12 02:54:08 +0530

    🚑 fix baseURL

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -1,8 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 export const instance: AxiosInstance = axios.create({
-  baseURL: `${
-    process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.NEXT_PUBLIC_BASE_URL
-  }/api/`,
+  baseURL: `/api/`,
 });
 
 export const postCode = async (to: string, data: {}): Promise<{}> => {
