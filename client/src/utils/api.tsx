487dbe841a6740fbd8a3198c5a707ee5c59acd3d
Snehil <snehilwebber@gmail.com>
2021-04-13 04:07:15 +0530

    ⚰️ Remove unused api handlers

--- client/src/utils/api.tsx
+++ client/src/utils/api.tsx
@@ -49,16 +49,6 @@ export const fetchCampaigns = async (): Promise<any> => {
   }
 };
 
-export const getCode = async (to: string, data: {}): Promise<{}> => {
-  try {
-    const res = await instance.get(`${to}`, data);
-    return res;
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-};
-
 const handleError = (msg: string) => {
   toast.error(msg);
 };
