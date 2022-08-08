3408136bc6fb52916f52d32720a05dcf4cbdbe73
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-08-09 05:14:29 +0530

    add basic profile page

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { HeroAnimation } from '../assets/icons';
+import { Layout } from '../components';
+import { useAuth } from '../store/authContext';
 
 const Profile: React.FC = () => {
-  return <div>Profile</div>;
+  const { signOut } = useAuth();
+  return (
+    <Layout background={HeroAnimation}>
+      <div className="flex items-center lg:flex-row flex-col max-w-full justify-between ">
+        <header className="text-title my-4">Profile</header>
+        <button className="actionBtn  " onClick={signOut}>
+          Log Out
+        </button>
+      </div>
+    </Layout>
+  );
 };
 
 export default Profile;
